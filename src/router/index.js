8da1729525334a29f1be694e3500fba02cd66677
Anggi Danu Saputra <danusaputra068@gmail.com>
2025-11-15 08:49:49 +0700

    feat: Configure Vue Router with all routes
    - Add home route (/)
    - Add product detail route (/products/:slug)
    - Add cart route (/cart)
    - Add checkout route (/checkout)
    - Add order success route (/order-success/:orderNumber)
    - Add scrollBehavior for better UX
    - Use lazy loading for all routes except home

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/products/:slug',
+      name: 'product-detail',
+      component: () => import('../views/ProductDetailView.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../views/CartView.vue')
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: () => import('../views/CheckoutView.vue')
+    },
+    {
+      path: '/order-success/:orderNumber',
+      name: 'order-success',
+      component: () => import('../views/OrderSuccessView.vue')
+    }
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
